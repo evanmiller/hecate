817d368eaea2d5dc34876048ba1bfd2f4231519c
Evan Miller <emmiller@gmail.com>
2015-04-25 10:02:20 -0500

    Support for relative (+/-) offsets

--- screen_data.go
+++ screen_data.go
@@ -66,6 +66,53 @@ func insertRuneAtIndex(value []byte, index int, newRuneValue rune) []byte {
 	return new_string[0:pos]
 }
 
+func (field_editor *FieldEditor) handleKeyEvent(event termbox.Event, file_pos int) int {
+	new_file_pos := -1
+	if event.Key == termbox.KeyEnter {
+		if len(field_editor.value) > 0 {
+			scanned_file_pos := 0
+			if n, _ := fmt.Sscanf(string(field_editor.value), "+%v", &scanned_file_pos); n > 0 {
+				new_file_pos = file_pos + scanned_file_pos
+			} else if n, _ := fmt.Sscanf(string(field_editor.value), "%v", &scanned_file_pos); n > 0 {
+				if scanned_file_pos < 0 {
+					if scanned_file_pos+file_pos < 0 {
+						new_file_pos = 0
+					} else {
+						new_file_pos = scanned_file_pos + file_pos
+					}
+				} else {
+					new_file_pos = scanned_file_pos
+				}
+			}
+		} else {
+			new_file_pos = file_pos
+		}
+	} else if event.Key == termbox.KeyEsc {
+		new_file_pos = file_pos
+	} else if event.Key == termbox.KeyArrowLeft {
+		if field_editor.cursor_pos > 0 {
+			field_editor.cursor_pos--
+		}
+	} else if event.Key == termbox.KeyArrowUp || event.Key == termbox.KeyCtrlA {
+		field_editor.cursor_pos = 0
+	} else if event.Key == termbox.KeyArrowRight {
+		if field_editor.cursor_pos < utf8.RuneCount(field_editor.value) {
+			field_editor.cursor_pos++
+		}
+	} else if event.Key == termbox.KeyArrowDown || event.Key == termbox.KeyCtrlE {
+		field_editor.cursor_pos = utf8.RuneCount(field_editor.value)
+	} else if event.Key == termbox.KeyCtrlH || event.Key == termbox.KeyBackspace {
+		if field_editor.cursor_pos > 0 {
+			field_editor.value = removeRuneAtIndex(field_editor.value, field_editor.cursor_pos-1)
+			field_editor.cursor_pos--
+		}
+	} else if unicode.IsPrint(event.Ch) {
+		field_editor.value = insertRuneAtIndex(field_editor.value, field_editor.cursor_pos, event.Ch)
+		field_editor.cursor_pos++
+	}
+	return new_file_pos
+}
+
 func (screen *DataScreen) handleKeyEvent(event termbox.Event) int {
 	modes := map[rune]CursorMode{
 		'i': IntegerMode,
@@ -78,36 +125,9 @@ func (screen *DataScreen) handleKeyEvent(event termbox.Event) int {
 	} else if event.Ch == '?' { // about
 		return ABOUT_SCREEN_INDEX
 	} else if screen.field_editor != nil {
-		if event.Key == termbox.KeyEnter {
-			if len(screen.field_editor.value) > 0 {
-				if n, _ := fmt.Sscanf(string(screen.field_editor.value), "%v", &screen.cursor.pos); n > 0 {
-					screen.field_editor = nil
-				}
-			} else {
-				screen.field_editor = nil
-			}
-		} else if event.Key == termbox.KeyEsc {
+		if new_pos := screen.field_editor.handleKeyEvent(event, screen.cursor.pos); new_pos >= 0 {
+			screen.cursor.pos = new_pos
 			screen.field_editor = nil
-		} else if event.Key == termbox.KeyArrowLeft {
-			if screen.field_editor.cursor_pos > 0 {
-				screen.field_editor.cursor_pos--
-			}
-		} else if event.Key == termbox.KeyArrowUp || event.Key == termbox.KeyCtrlA {
-			screen.field_editor.cursor_pos = 0
-		} else if event.Key == termbox.KeyArrowRight {
-			if screen.field_editor.cursor_pos < utf8.RuneCount(screen.field_editor.value) {
-				screen.field_editor.cursor_pos++
-			}
-		} else if event.Key == termbox.KeyArrowDown || event.Key == termbox.KeyCtrlE {
-			screen.field_editor.cursor_pos = utf8.RuneCount(screen.field_editor.value)
-		} else if event.Key == termbox.KeyCtrlH || event.Key == termbox.KeyBackspace {
-			if screen.field_editor.cursor_pos > 0 {
-				screen.field_editor.value = removeRuneAtIndex(screen.field_editor.value, screen.field_editor.cursor_pos-1)
-				screen.field_editor.cursor_pos--
-			}
-		} else if unicode.IsPrint(event.Ch) {
-			screen.field_editor.value = insertRuneAtIndex(screen.field_editor.value, screen.field_editor.cursor_pos, event.Ch)
-			screen.field_editor.cursor_pos++
 		}
 	} else if event.Ch == ':' {
 		screen.field_editor = new(FieldEditor)
