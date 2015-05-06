9193ce56df39f6ad6a6e9c688d17266ba94ecb95
Florian Sundermann <boombuler@googlemail.com>
2015-05-06 15:17:51 +0200

    ported to windows

--- style.go
+++ style.go
@@ -23,27 +23,6 @@ type Style struct {
 
 	field_editor_bg termbox.Attribute
 	field_editor_fg termbox.Attribute
-}
 
-func defaultStyle() Style {
-	var style Style
-	style.default_bg = termbox.Attribute(1)
-	style.default_fg = termbox.Attribute(256)
-	style.rune_fg = termbox.Attribute(248)
-	style.int_fg = termbox.Attribute(154)
-	style.bit_fg = termbox.Attribute(154)
-	style.space_rune_fg = termbox.Attribute(240)
-
-	style.text_cursor_hex_bg = termbox.Attribute(167)
-	style.bit_cursor_hex_bg = termbox.Attribute(26)
-	style.int_cursor_hex_bg = termbox.Attribute(63)
-	style.fp_cursor_hex_bg = termbox.Attribute(127)
-
-	style.hilite_hex_fg = termbox.Attribute(231)
-	style.hilite_rune_fg = termbox.Attribute(256)
-
-	style.field_editor_bg = style.default_fg
-	style.field_editor_fg = style.default_bg
-
-	return style
+	about_logo_bg termbox.Attribute
 }
