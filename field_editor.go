2038852492d1ac6a6750a90be10d52d73ba0263d
Andreas Stenius <andreas.stenius@svenskaspel.se>
2016-01-17 10:03:17 +0100

    field_editor: overwrite mode and fixed max input length.

--- field_editor.go
+++ field_editor.go
@@ -13,6 +13,8 @@ type FieldEditor struct {
 	cursor_pos int
 	last_value string
 	width      int
+	fixed      int
+	overwrite  bool
 }
 
 func (field_editor *FieldEditor) handleKeyEvent(event termbox.Event) (string, bool) {
@@ -44,14 +46,27 @@ func (field_editor *FieldEditor) handleKeyEvent(event termbox.Event) (string, bo
 			field_editor.value = removeRuneAtIndex(field_editor.value, field_editor.cursor_pos-1)
 			field_editor.cursor_pos--
 		}
+	} else if event.Key == termbox.KeyCtrlK {
+		field_editor.cursor_pos = 0
+		field_editor.value = make([]byte, 0)
 	} else if unicode.IsPrint(event.Ch) {
-		field_editor.value = insertRuneAtIndex(field_editor.value, field_editor.cursor_pos, event.Ch)
-		field_editor.cursor_pos++
+		field_editor.insert(event.Ch)
 	} else if event.Key == termbox.KeySpace {
-		field_editor.value = insertRuneAtIndex(field_editor.value, field_editor.cursor_pos, ' ')
-		field_editor.cursor_pos++
+		field_editor.insert(' ')
 	}
 	return string(field_editor.value), is_done
+}
+
+func (field_editor *FieldEditor) insert(r rune) {
+	if field_editor.overwrite && field_editor.cursor_pos < utf8.RuneCount(field_editor.value) {
+		field_editor.value[field_editor.cursor_pos] = byte(r)
+	} else {
+		if field_editor.fixed > 0 && field_editor.cursor_pos == field_editor.fixed {
+			return
+		}
+		field_editor.value = insertRuneAtIndex(field_editor.value, field_editor.cursor_pos, r)
+	}
+	field_editor.cursor_pos++
 }
 
 func (field_editor *FieldEditor) drawFieldValueAtPoint(style Style, x, y int) int {
