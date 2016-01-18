be7bf5daaa02880b7b05fa3a0cc134ba8d740b8e
Andreas Stenius <andreas.stenius@svenskaspel.se>
2016-01-18 22:08:47 +0100

    field_editor: refactor to use []rune instead of []byte.
    to filter out invalid inputs.

--- string_util.go
+++ string_util.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"unicode/utf8"
-
 	"github.com/nsf/termbox-go"
 )
 
@@ -25,44 +23,6 @@ func drawStringAtPoint(str string, x int, y int, fg termbox.Attribute, bg termbo
 		x_pos++
 	}
 	return x_pos - x
-}
-
-func removeRuneAtIndex(value []byte, index int) []byte {
-	var runes []rune
-	new_string := make([]byte, utf8.UTFMax*(len(value)+1))
-	pos := 0
-	for _, runeValue := range string(value) {
-		if pos != index {
-			runes = append(runes, runeValue)
-		}
-		pos++
-	}
-	pos = 0
-	for _, runeValue := range runes {
-		pos += utf8.EncodeRune(new_string[pos:], runeValue)
-	}
-	return new_string[0:pos]
-}
-
-func insertRuneAtIndex(value []byte, index int, newRuneValue rune) []byte {
-	var runes []rune
-	new_string := make([]byte, utf8.UTFMax*(len(value)+1))
-	pos := 0
-	for _, runeValue := range string(value) {
-		if pos == index {
-			runes = append(runes, newRuneValue)
-		}
-		runes = append(runes, runeValue)
-		pos++
-	}
-	if index == pos {
-		runes = append(runes, newRuneValue)
-	}
-	pos = 0
-	for _, runeValue := range runes {
-		pos += utf8.EncodeRune(new_string[pos:], runeValue)
-	}
-	return new_string[0:pos]
 }
 
 func binaryStringForInteger8(value uint8) string {
